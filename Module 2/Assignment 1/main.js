082d0895045ada2f8448734211a54ae0311d8ba8
Whytebread <lukeweisbrot@gmail.com>
2025-04-25 09:35:14 -0500

    added comments

--- Module 2/Assignment 1/main.js
+++ Module 2/Assignment 1/main.js
@@ -14,29 +14,31 @@ nameRegex = /^[A-Z]/;
 
 // function to validate form input
 function validateInputForm(form) {
-    if (nameRegex.test(form.firstName.value) === false) {
+    if (nameRegex.test(form.firstName.value) === false) { //checks if the first name input starts with a capital letter
         alert("First Name must start with a capital letter")
         return false;
     }
 
-    if (nameRegex.test(form.lastName.value) === false) {
+    if (nameRegex.test(form.lastName.value) === false) { //checks if the last name input starts with a capital letter
         alert("Last Name must start with a capital letter")
         return false;
     }
     
-    if (dateRegex.test(form.requestDate.value) === false) {
+    if (dateRegex.test(form.requestDate.value) === false) { //checks if the date is in mm/dd/yyyy format
         alert("Date must be mm/dd/yyyy format")
         return false;
     }
 
-    if (employeeRegex.test(form.employeeID.value) === false) {
+    // checks if the employee ID input is 6 alphanumeric characters, starting with a capital letter, followed by 5 numbers
+    if (employeeRegex.test(form.employeeID.value) === false) { 
         alert("Employee ID must be 6 alphanumeric characters, starting with a capital letter, followed by 5 numbers")
         return false;
     }
 
-    return alert("Form submission successful!");
+    return alert("Form submission successful!"); //alerts if all input validation checks are valid
 }
 
+// even listener that first prevents default of submitting the form before validation checks and then executes the validateInputForm function passing in the form inputs
 document.getElementById("submitButton").addEventListener("click", function(event) {
     event.preventDefault();
     validateInputForm(inputForm);
